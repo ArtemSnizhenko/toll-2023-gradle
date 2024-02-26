11751cc5e0cc5a277d72218cdfb54c37f62e2c0e
artem.snizhenko <artem.snizenko@gmail.com>
2024-02-26 09:30:19 +0700

    Реализация платформы Spting MVC. Работа над ошибками

--- tracker-core/src/main/java/jdev/tracker/app/InjectionContext.java
+++ tracker-core/src/main/java/jdev/tracker/app/InjectionContext.java
@@ -3,8 +3,6 @@ package jdev.tracker.app;
 import jdev.tracker.service.GPSDataService;
 import jdev.tracker.service.DataSaveService;
 import jdev.tracker.service.DataSendService;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
--- tracker-core/src/main/java/jdev/tracker/app/TrackerApplication.java
+++ tracker-core/src/main/java/jdev/tracker/app/TrackerApplication.java
@@ -3,6 +3,7 @@ package jdev.tracker.app;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by artem on 27.12.23.
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 /*анотация Spring Boot*/
 @SpringBootApplication
+/*@ComponentScan({"app","service"})*/
 public class TrackerApplication {
 
     public static void main(String... args) throws Exception {
